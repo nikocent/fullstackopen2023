c8d327114d72dcb8ae49e33fda80f1ac8e87e09b
nikocent <nicocenteno202@gmail.com>
2023-07-05 18:04:39 -0300

    Adapted App to the latest changes (steps 3, 4 and 5)

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <h1>{props.course.name}</h1>
+)
+}
+
+const Content = (props) => {
+  let [part0, part1, part2] = props.parts
+  return (
+    <div>
+      <p>{part0.name} has {part0.exercises} exercises</p>
+      <p>{part1.name} has {part1.exercises} exercises</p>
+      <p>{part2.name} has {part2.exercises} exercises</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let total = props.parts
+    .map((a) => a.exercises)
+    .reduce((a, b) => a + b)
+
+  return (
+    <div>
+      Total exercises: {total}
+    </div>
+  )
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -22,8 +51,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
