e3cfced9e1bb9a1adcc7ed4c297c680738d8d378
nikocent <nicocenteno202@gmail.com>
2023-07-05 15:42:14 -0300

    Replacing App's body (Step 1 and 2)

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -12,17 +12,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Part ... />
+      <Part ... />
+      <Part ... />
     </div>
   )
 }
