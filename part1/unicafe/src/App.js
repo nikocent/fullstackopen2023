1db860420c3f002324fca31d44abce68c63f41c7
Nicolás Centeno O <nicocenteno202@gmail.com>
2023-07-06 23:30:10 -0300

    Added Total, Average and Positive percentage statistics (2nd step)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = (good - bad) / total
+  const positive = (good / total) * 100
 
   return (
     <div>
@@ -17,6 +21,9 @@ const App = () => {
         <li>Good: {good}</li>
         <li>Neutral: {neutral}</li>
         <li>Bad: {bad}</li>
+        <li>All: {total}</li>
+        <li>Average: {average}</li>
+        <li>Positive: {positive}%</li>
       </ul>
     </div>
   )
