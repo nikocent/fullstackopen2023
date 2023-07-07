0c11d071ebe59c98f64ee756ed459578654b1647
Nicolás Centeno O <nicocenteno202@gmail.com>
2023-07-07 00:12:08 -0300

    Added table and tbody HTML tags to display data as a table (6th step)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -16,15 +16,16 @@ const Statistics = ({good, neutral, bad}) => {
   const average = (good - bad) / total
   const positive = (good / total) * 100
   return (
-    <div>
-      <h1>Statistics</h1>
+    <table>
+      <tbody>
       <StatisticLine text={'Good: '} value={good}/>
       <StatisticLine text={'Neutral: '} value={neutral}/>
       <StatisticLine text={'Bad: '} value={bad}/>
       <StatisticLine text={'All: '} value={total}/>
       <StatisticLine text={'Average: '} value={average}/>
       <StatisticLine text={'Positive: '} value={positive}/>
-    </div>
+      </tbody>
+    </table>
   )
 }
 
@@ -39,6 +40,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text='Good'/>
       <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral'/>
       <Button handleClick={() => setBad(bad + 1)} text='Bad'/>
+      <h1>Statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
