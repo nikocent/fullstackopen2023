3721b44f9b55905b842a98b4688d15d430f77668
Nicolás Centeno O <nicocenteno202@gmail.com>
2023-07-06 23:23:53 -0300

    First code for unicafe feedback app (1st step)

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback!</h1>
+      <Button handleClick={() => setGood(good + 1)} text='Good'/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text='Neutral'/>
+      <Button handleClick={() => setBad(bad + 1)} text='Bad'/>
+      <h1>Statistics</h1>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
