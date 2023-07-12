75aa26a313aaec1af58dacb17c13a56450c6b587
Nicolás Centeno O <nicocenteno202@gmail.com>
2023-07-12 10:52:32 -0300

    Fixed duplicated alert and implemented phone number input (step 3)

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,26 +2,32 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas',
+      phone: '040-1234567',
+      id: 1
+    }
+  ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
-  const handleNewName = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleNewName = (event) => setNewName(event.target.value)
+  const handleNewPhone = (event) => setNewPhone(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      phone: newPhone,
+      id: persons.slice(-1)[0].id + 1
     }
-    if (persons
-      .map(item => JSON.stringify(item))
-      .indexOf(JSON.stringify(newPerson)) === -1)
-    {
+    const isDuplicated = (persons.find(item => item.name === newName) !== undefined)
+    if (!isDuplicated){
       setPersons(persons.concat(newPerson))
       setNewName('')
-    } else {alert(`${newName} is already added to the phonebook`)}
+      setNewPhone('')
+    } else {
+      alert(`${newName} is already added to the phonebook`)
+    }
   }
 
   return (
@@ -34,12 +40,16 @@ const App = () => {
           value={newName}/>
         </div>
         <div>
+          number: <input
+          onChange={handleNewPhone}
+          value={newPhone}/></div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(item => <p>{item.name}</p>)}
-      <div>debug: {newName}</div>
+        {persons.map(item => <p key={item.id}>{item.name} {item.phone}</p>)}
+      <div>debug: {newName} {newPhone}</div>
     </div>
   )
 }
